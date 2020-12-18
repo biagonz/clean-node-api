960dc01918def8c45e260d3daa6c4332fcde651f
Beatriz Silva <beatriz.silva@objective.com.br>
2020-12-18 16:41:10 -0300

    refactor: simplyfy code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,11 +4,11 @@ import { HttpResponse, HttpResquest } from '../protocols/http'
 
 export class SingUpController {
   handle (httpRequest: HttpResquest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 }
